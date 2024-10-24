e9f50bf9555a46c8b6b9e54d63731ca77e946068
HudsonPWesel <cswyld7k@gmail.com>
2024-10-24 18:16:51 -0400

    Clean-up repo

--- socket-practice.c
+++ socket-practice.c
@@ -5,26 +5,10 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
-// struct Animal
-// {
-//   int num_legs;
-// };
-
-// struct Dog
-// {
-//   int legs;
-//   int tail_length;
-// };
-
-// int get_legs(struct Animal *an)
-// {
-//   printf("%d", an->num_legs);
-//   return an->num_legs;
-// }
-
 int main(int argc, char const *argv[])
 
-{ // Create socket
+{
+  // Create socket
   // attach local IP and port to socket (bind)
   // announce abillity to accept conn (listen)
   // Block incoming conn until con request arrives (accept)
@@ -37,21 +21,10 @@ int main(int argc, char const *argv[])
   int remote = socket(AF_INET, SOCK_STREAM, 0);         // Create socket (IPv4 and TCP)
   bind(remote, (struct sockaddr *)&addr, sizeof(addr)); // Bind socket to ADDR and PORT
   listen(remote, 3);                                    // Listen for conn
-  int size;
-  char *text = NULL;
-  scanf("%d", &size);
-  text = (char *)malloc(size * sizeof(char));
-  printf("Enter Size: ");
-
-  printf("\n Enter Text: ");
-  scanf("%s", text);
-
   while (1)
   {
     printf("%s", "Waiting for incoming connections...\n");
     int clientLen = sizeof(struct sockaddr_in *); // IDK max input size
     int new_socket = accept(remote, (struct sockaddr *)&addr, &clientLen);
-
-    send(new_socket, text, sizeof(text), 0);
   }
 }
