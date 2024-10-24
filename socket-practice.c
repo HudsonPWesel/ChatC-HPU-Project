b7f725c992e7b3f88d43bc79754a96d29062afef
HudsonPWesel <cswyld7k@gmail.com>
2024-10-24 17:50:50 -0400

    Compressed version

--- socket-practice.c
+++ socket-practice.c
@@ -29,31 +29,21 @@ int main(int argc, char const *argv[])
   // announce abillity to accept conn (listen)
   // Block incoming conn until con request arrives (accept)
   // Establish connection (connect )
+  struct sockaddr_in addr = {0};
+  addr.sin_family = AF_INET;
+  addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  addr.sin_port = htons(12345);
 
-  // struct Dog rover = {0};
-
-  // rover.legs = 10;
-  // rover.tail_length = 10;
-
-  // get_legs((struct Animal *)&rover);
-
-  struct sockaddr_in remote = {0};
-  int clientPort = 12345;
-
-  remote.sin_family = AF_INET;
-  remote.sin_addr.s_addr = htonl(INADDR_ANY);
-  remote.sin_port = htons(clientPort);
-
-  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-  bind(sockfd, (struct sockaddr *)&remote, sizeof(remote));
-  listen(sockfd, 3);
+  int remote = socket(AF_INET, SOCK_STREAM, 0);
+  bind(remote, (struct sockaddr *)&addr, sizeof(addr));
+  listen(remote, 3);
 
   while (1)
   {
     printf("%s", "Waiting for incoming connections...\n");
     char buffer[1024];
     int clientLen = sizeof(struct sockaddr_in *);
-    int new_socket = accept(sockfd, (struct sockaddr *)&remote, &clientLen);
+    int new_socket = accept(remote, (struct sockaddr *)&addr, &clientLen);
 
     ssize_t valread = read(new_socket, buffer, 1024 - 1);
     printf("%s", buffer);
